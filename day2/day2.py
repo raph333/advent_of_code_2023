f949ebc7363c4a9ee510da79af62dc607b29261d
Raphael <raphael1peer@gmail.com>
2024-01-28 13:06:03 +0200

    unnest and refactor

--- day2/day2.py
+++ day2/day2.py
@@ -1,22 +1,40 @@
-CUBES = {"red": 12, "green": 13, "blue": 14}
+from collections import defaultdict
+from enum import Enum
+from typing import List, Dict
 
-with open("input.txt", "r") as f:
-    lines = f.readlines()
+
+class Color(Enum):
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
 
 
-possible_games = []
+DrawDict = Dict[Color, int]
+CUBES = {Color.RED: 12, Color.GREEN: 13, Color.BLUE: 14}
 
-for line in lines:
-    game_id, games = line.replace("Game", "").strip().split(":")
-    for draw in games.split(";"):
-        for cube in draw.split(","):
-            num, color = cube.strip().split(" ")
-            if int(num) > CUBES[color]:
-                break
-        else:
-            continue  # move to next draw if cube-loop didn't break
-        break
-    else:
-        possible_games.append(int(game_id))  # if game-loop didn't break
 
-print(possible_games), sum(possible_games)
+def read_data(file_path: str) -> Dict[int, List[DrawDict]]:
+    with open(file_path) as f:
+        lines = f.readlines()
+    data = defaultdict(list)
+
+    for line in lines:
+        game, draws = line.split(":")
+        game_id = int(game.replace("Game", "").strip())
+        for draw in draws.strip().split(";"):
+            cubes = [cube.strip().split(" ") for cube in draw.split(",")]
+            data[game_id].append({Color(color): int(num) for num, color in cubes})
+
+    return dict(data)
+
+
+def draw_possible(draw: DrawDict) -> bool:
+    return all(num <= CUBES[color] for color, num in draw.items())
+
+
+def game_possible(game: List[DrawDict]) -> bool:
+    return all(draw_possible(d) for d in game)
+
+
+games = read_data("input.txt")
+print(sum(id_ for id_, game in games.items() if game_possible(game)))
